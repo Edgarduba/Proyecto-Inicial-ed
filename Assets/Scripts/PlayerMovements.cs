a15e08d7ffb0de4a7856f4b4b06154959f1b9acf
Edgarduba <edgarduranbadell@gmail.com>
2022-10-19 09:00:17 +0200

    mondongo

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,42 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMovements : MonoBehaviour
 {
-    public PlayerMovements controller;
-    public float speed = 5f;
-    float dirX;
-    Rigidbody2D _rBody;
+    //public PlayerMovements controller;
+    private CharacterController controller;
+    public float runSpeed = 2f;
+    private Rigidbody2D body;
+    private float horizontal;
+    public PlayableDirector director;
+    private Animator anim;
 
 
-    void Awake()
+
+    public float sensorRadius = 0.1f;
+    private Vector2 playerVelocity;
+    public Transform groundSensor;
+    public LayerMask ground;
+    public float jumpForce = 20;
+    public float jumpHeight = 1;
+    private float gravity = -9.81f;
+    public bool isGrounded;
+
+
+
+    void Jump()
     {
-        
-        
-        _rBody = GetComponent<Rigidbody2D>();
-        
+        //isGrounded = controller.isGrounded;
+        isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //playerVelocity.y += jumpForce;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    
+
+
+    void Start() 
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    
+      
     void Update()
     {
-        dirX = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
-        Debug.Log(dirX);
+        if(horizontal == 0)
+        {
+            anim.SetBool("Correr", false);
+        }
+        else
+        {
+            anim.SetBool("Correr", true);
+        }
+
+        Jump();
+        
     }
 
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        _rBody.velocity = new Vector2(dirX * speed, _rBody.velocity.y);
-
+        body.velocity = new Vector2(horizontal * runSpeed, 0);
     }
 
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "Cinematica")
+        {
+            director.Play();
+        }
+        
+    }
+    
 
 
 
-/*
+
+ /*
     private Transform playerTransform;
     
     void Start()
